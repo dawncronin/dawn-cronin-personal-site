01ce963cac286fd6a74177f416f596eeaf71d4a0
Dawn Cronin <dawncronin7@gmail.com>
2021-03-15 11:31:07 -0700

    added spoiled project

--- index.js
+++ index.js
@@ -44,3 +44,52 @@ rightArrow.addEventListener("click", () => {
         }
     })
 })
+
+// Spoiled arrows
+
+
+let leftArrow2 = document.querySelector(".left-2")
+let rightArrow2 = document.querySelector(".right-2")
+
+let images2 = document.querySelectorAll(".simage")
+
+let curPosition2 = 0
+
+images2.forEach((image, i) => {
+    if (i === curPosition2) {
+        image.style.display = "inline-block"
+    } else {
+        image.style.display = "none"
+    }
+})
+
+leftArrow2.addEventListener("click", () => {
+    curPosition2 -= 1
+    if (curPosition2 >= images2.length) {
+        curPosition2 = curPosition2%images2.length
+    }
+    if (curPosition2 < 0) {
+        curPosition2 = images2.length + curPosition2
+    }
+    images2.forEach((image, i) => {
+        if (i === curPosition2) {
+            image.style.display = "inline-block"
+        } else {
+            image.style.display = "none"
+        }
+    })
+})
+
+rightArrow2.addEventListener("click", () => {
+    curPosition2 += 1
+    if (curPosition2 >= images2.length) {
+        curPosition2 = curPosition2%images2.length
+    }
+    images2.forEach((image, i) => {
+        if (i === curPosition2) {
+            image.style.display = "inline-block"
+        } else {
+            image.style.display = "none"
+        }
+    })
+})
